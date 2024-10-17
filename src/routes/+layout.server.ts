aec06e5fe687da7a12e37dd3d4c3a48e6b4f3ee6
Jacob Singer <jacobleesinger@gmail.com>
2024-10-17 12:38:59 -0500

    feat: add supabase auth

---
+++ src/routes/+layout.server.ts
@@ -0,0 +1,11 @@
+// layout.server.ts
+
+import type { LayoutServerLoad } from './$types'
+
+export const load: LayoutServerLoad = async ({ locals: { safeGetSession }, cookies }) => {
+  const { session } = await safeGetSession()
+  return {
+    session,
+    cookies: cookies.getAll(),
+  }
+}
