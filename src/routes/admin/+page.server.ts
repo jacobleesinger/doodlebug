6e313cf137afaef9fd3441a4c06875020c18ac32
Jacob Singer <jacobleesinger@gmail.com>
2024-10-21 14:51:02 -0500

    feat: add superforms

--- src/routes/admin/+page.server.ts
+++ src/routes/admin/+page.server.ts
@@ -1,5 +1,29 @@
+import { superValidate } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
+import { z } from 'zod';
+
+const memorySchema = z.object({
+  slug: z.string(),
+  caption: z.optional(z.string()),
+  image: z.instanceof(File, { message: 'image is required'}),
+  imageAltText: z.string(),
+})
+
 import type { PageServerLoad } from './$types';
 
 export const load = (async () => {
-    return {};
+  const form = await superValidate(zod(memorySchema));
+  return {
+    form
+  };
 }) satisfies PageServerLoad;
+
+export const actions = {
+  default: async ({ request }) => {
+    const formData = await request.formData();
+    console.log('formData', formData);
+    const form = await superValidate(formData, zod(memorySchema));
+
+    console.log('form', form);
+  }
+}
--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,24 +1,30 @@
 <script lang="ts">
+    import SuperDebug, { superForm, fileProxy } from 'sveltekit-superforms';
     import type { PageData } from './$types';
     
     export let data: PageData;
+
+    const { form } = superForm(data.form);
+
+    console.log('form', form);
+
+    const file = fileProxy(form, 'image');
 </script>
 
 <main>
-  <form class="flex flex-col justify-start items-start" method="POST">
-    <label>
-      Slug
-      <input type="text" name="slug" required class="input input-bordered"/>
-    </label>
-    <label>
-      Caption
-      <textarea name="caption" class="textarea textarea-bordered"></textarea>
-    </label>
+  <SuperDebug data={$form} />
+  <form class="flex flex-col justify-start items-start" method="POST" enctype="multipart/form-data">
+    <label for="slug ">Slug</label>
+    <input type="text" name="slug" bind:value={$form.slug} required class="input input-bordered"/>
 
-    <label>
-      Image
-      <input type="file" accept="img/*" name="image" class="input"/>
-    </label>
+    <label for="caption">Caption</label>
+    <textarea name="caption" bind:value={$form.caption} class="textarea textarea-bordered"></textarea>
+
+    <label for="image">Image</label>
+    <input type="file" accept="img/png, img/jpeg" name="image" bind:files={$file} class="input"/>
+
+    <label for="imageAltText">Image Alt Text</label>
+    <input type="text" name="imageAltText" bind:value={$form.imageAltText} required class="input input-bordered"/>
 
     <button class="btn btn-primary">Save</button>
   </form>
