fc60e0387d10b7268a1de1959047d5bdc39b8e18
Jacob Singer <jacobleesinger@gmail.com>
2024-11-19 14:02:26 -0600

    feat: move logout to main layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,28 @@
 <script lang="ts">
-    import '../app.css';
+  import { goto } from '$app/navigation';
+  import '../app.css';
 
-    let { children } = $props();
+  let { children, data  } = $props();
+
+  let { supabase, user } = $derived(data);
+
+  const logout = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      console.error(error);
+    } else {
+      goto('/auth')
+    }
+  };
 </script>
 
 <header>
-  <h1 class="text-8xl">Doodlebug</h1>
+  <a href="/"><h1 class="text-8xl">Doodlebug</h1></a>
 </header>
+
+{#if user}
+  <button onclick={logout}>Log Out</button>
+{/if}
 
 {#if children}
   {@render children()}
--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -1,27 +1,9 @@
 <script lang="ts">
-  const { children, data  } = $props();
-  import { goto } from '$app/navigation';
+  const { children  } = $props();
 
-    const { supabase } = $derived(data);
-
-    const logout = async () => {
-
-      const { error } = await supabase.auth.signOut();
-      if (error) {
-        console.error(error);
-      } else {
-        goto('/auth')
-      }
-      
-    };
 </script>
 
-<header>
-	<nav>
-		<a href="/">Home</a>
-	</nav>
-	<button onclick={logout}>Logout</button>
-</header>
+
 <main>
 	{#if children}
     {@render children()}
