865f0179400155443bc18b182d865c617195d06d
Jacob Singer <jacobleesinger@gmail.com>
2024-11-19 13:39:36 -0600

    feat: move header to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
     import '../app.css';
-    
+
     let { children } = $props();
 </script>
+
+<header>
+  <h1 class="text-8xl">Doodlebug</h1>
+</header>
 
 {#if children}
   {@render children()}
