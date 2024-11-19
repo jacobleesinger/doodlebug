0c79093dfcc91bc9f2b5ceffe34b9f3d651df9a2
Jacob Singer <jacobleesinger@gmail.com>
2024-11-19 15:15:03 -0600

    feat: styling for new memory form

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import SuperDebug, { superForm, fileProxy } from 'sveltekit-superforms';
+    import { superForm, fileProxy } from 'sveltekit-superforms';
     import type { PageData } from './$types';
     
     export let data: PageData;
@@ -11,25 +11,37 @@
     const file = fileProxy(form, 'image');
 </script>
 
-<main>
-  <SuperDebug data={$form} />
+<main class="flex flex-col justify-center items-center container">
+   <h2 class="text-2xl my-8">Add Memory</h2>
   <form class="flex flex-col justify-start items-start" method="POST" enctype="multipart/form-data">
-    <label for="title ">Title</label>
-    <input type="text" name="title" bind:value={$form.title} required class="input input-bordered"/>
+    <section class="flex flex-col space-y-2 mb-4">
+      <label for="title ">Title</label>
+      <input type="text" name="title" bind:value={$form.title} required class="input input-bordered"/>
+    </section>
 
-    <label for="slug ">Slug</label>
-    <input type="text" name="slug" bind:value={$form.slug} required class="input input-bordered"/>
+    <section class="flex flex-col space-y-2 mb-4">
+      <label for="slug ">Slug</label>
+      <input type="text" name="slug" bind:value={$form.slug} required class="input input-bordered"/>
+    </section>
 
-    <label for="caption">Caption</label>
-    <textarea name="caption" bind:value={$form.caption} class="textarea textarea-bordered"></textarea>
+    <section class="flex flex-col space-y-2 mb-4">
+      <label for="caption">Caption</label>
+      <textarea name="caption" bind:value={$form.caption} class="textarea textarea-bordered"></textarea>
+    </section>
 
-    <label for="image">Image</label>
-    <input type="file" accept="img/png, img/jpeg" name="image" bind:files={$file} class="input"/>
+    <section class="flex flex-col space-y-2 mb-4">
+      <label for="image">Image</label>
+      <input type="file" accept="img/png, img/jpeg" name="image" bind:files={$file} class="input"/>
+    </section>
 
-    <label for="imageAltText">Image Alt Text</label>
-    <input type="text" name="imageAltText" bind:value={$form.imageAltText} required class="input input-bordered"/>
+    <section class="flex flex-col space-y-2 mb-4">
+      <label for="imageAltText">Image Alt Text</label>
+      <input type="text" name="imageAltText" bind:value={$form.imageAltText} required class="input input-bordered"/>
+    </section>
 
-    <button class="btn btn-primary">Save</button>
+    <section class="flex mt-4">
+        <button class="btn btn-primary">Save</button>
+    </section>
   </form>
 </main>
 
